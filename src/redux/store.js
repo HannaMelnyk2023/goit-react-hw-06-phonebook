d5d79e08c6480d5cd2f17c9ee73bb440cc867159
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2026-02-02 22:06:58 +0200

    Update Mistakes in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { contactsReduser } from './contactsSlice';
 import { filterReducer } from './filterSlice';
@@ -18,6 +27,19 @@ export const store = configureStore({
         contacts: persistedContactsReduser,
         filter: filterReducer,
     },
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ],
+            },
+        }),
 });
 
 export const persistor = persistStore(store);
