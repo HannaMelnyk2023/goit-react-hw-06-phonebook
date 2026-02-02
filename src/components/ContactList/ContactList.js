dbc171f2d708e58ce2e3a7ee1fdbe0d6710dfa98
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2026-02-02 22:34:58 +0200

    trouble in form

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,8 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice';
 const ContactList = () => {
     const dispatch = useDispatch();
+
     const contacts = useSelector(state => state.contacts);
     const filter = useSelector(state => state.filter);
+
+    if (!Array.isArray(contacts)) {
+        return null;
+    }
 
     const visibleContacts = contacts.filter(contact =>
         contacts.name.toLowerCase().Includes(filter.toLowerCase())
@@ -22,5 +27,7 @@ const ContactList = () => {
         </ul>
     );
 };
+
+
 
 export default ContactList;
